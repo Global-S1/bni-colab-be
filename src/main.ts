83bccaf4ae744b6634dca712f31fcb83220f7f7d
Daniel Boggiano <dev@danielboggiano.com>
2026-09-21 00:42:13 -0500

    fix: place both db and be exclusively on coolify network with aliases to fix asymmetric routing and bind 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -29,7 +29,7 @@ async function bootstrap() {
   SwaggerModule.setup('api/docs', app, document);
 
   const port = process.env.PORT || 3002;
-  await app.listen(port);
+  await app.listen(port, '0.0.0.0');
   console.log(`🚀 BNITECH Colab Backend iniciado en: http://localhost:${port}`);
   console.log(`📄 Documentación Swagger disponible en: http://localhost:${port}/api/docs`);
 }
